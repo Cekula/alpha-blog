18f6f1d74510325b1173c25bdf16a5d6c1873ce1
Cekula <zafiiboii@gmail.com>
2023-03-16 19:02:56 +0100

    Added index for useres

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
    @articles = @user.articles
  end
+
+ def index
+  @users = User.all
+ end
  
  def edit
     @user = User.find(params[:id])
