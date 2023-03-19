47c36c247e9f0955d3bdbd3d97f647089f38398a
Cekula <zafiiboii@gmail.com>
2023-03-19 21:05:48 +0100

    integrated test for invalid category

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -12,4 +12,16 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+  test "get new catgeory form and reject invalid category submission" do
+    get "/catgeories/new"
+    assert_response :success
+    assert_no_difference 'Catgeory.count' do
+      post categories_path, params: { category: { name: " " } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+
 end
